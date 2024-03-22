20f5c44f248e22fdc1caad2c73a12fab2fdb848a
Cristhian Arce <cristhianarce30@gmail.com>
2024-03-21 21:18:03 -0500

    First Commit

---
+++ src/main/java/com/atpos/atpos/AtposG1Application.java
@@ -0,0 +1,13 @@
+package com.atpos.atpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtposG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtposG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/atpos/atpos/AtposG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atpos.atpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtposG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
