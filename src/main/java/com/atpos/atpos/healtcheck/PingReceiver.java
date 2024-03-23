966edf44132d2d897944c7dddc089f5dd4600797
Cristhian Arce <cristhianarce30@gmail.com>
2024-03-23 10:08:04 -0500

    Implement ping receiver

--- src/main/java/com/atpos/atpos/healtcheck/PingReceiver.java
+++ src/main/java/com/atpos/atpos/healtcheck/PingReceiver.java
@@ -1,4 +1,22 @@
 package com.atpos.atpos.healtcheck;
 
+import com.atpos.atpos.utils.ATPosServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PingReceiver {
+
+    private final HealthCheckService healthCheckService;
+
+    public PingReceiver(HealthCheckService healthCheckService) {
+        this.healthCheckService = healthCheckService;
+    }
+
+
+    @GetMapping("/health")
+    public ATPosServerResponse healthCheck() throws InterruptedException {
+        return healthCheckService.check();
+    }
 }
